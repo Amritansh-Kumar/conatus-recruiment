5029fe072a7fd02a19d61f1e27eee590f6631dab
Amritansh-Kumar <yadavamritanshami@gmail.com>
2019-06-23 20:10:33 +0530

    added cors support

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const auth = require('./middleware/auth-middleware');
 const userController = require('./controllers/user-controller');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
