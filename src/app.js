fb71121dbaca9a392855a1626836511e017ef473
Amritansh-Kumar <yadavamritanshami@gmail.com>
2019-08-07 19:49:11 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,32 @@ const answerController = require('./controllers/answer-controller');
 
 const app = express();
 
-app.use(cors());
+// CORS Setup
+const allowedOrigins = [
+    "https://teamconatus.com",
+];
+
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(undefined, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: [
+        "GET",
+        "HEAD",
+        "PUT",
+        "PATCH",
+        "POST",
+        "DELETE"
+    ]
+}));
+app.options("*");
+
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -35,7 +60,7 @@ app.get('/users/:user_id/answers', admin, userController.indexAnswers);
 /**
  * question routes
  */
-app.get('/questions', auth,questionController.indexQuestion);
+app.get('/questions', auth, questionController.indexQuestion);
 
 app.post('/questions', admin, questionController.storeQuestion);
 
@@ -44,7 +69,7 @@ app.post('/questions', admin, questionController.storeQuestion);
  */
 app.post('/answers', auth, answerController.storeAnswer);
 
-app.post('/answersupdate', auth , answerController.BulkUpdateAnswers);
+app.post('/answersupdate', auth, answerController.BulkUpdateAnswers);
 
 module.exports = app;
 
